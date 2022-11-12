7bbb62e3d1f520618105eed54e22f52344443c7f
tommyho973 <59982850+tommyho973@users.noreply.github.com>
2022-11-12 17:30:10 -0500

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,38 @@
+var dict = new Object();
+function string2array(data){
+    var arr = [];
+    var str3 = data;
+    while(str3.includes(";")){
+        var str1 = str3.substring(0,data.indexOf(";"));
+        arr.push(str1);
+        var str3 = str3.substring(str3.indexOf(";")+1);
+    }
+    return arr;
+}
+
+function getData(form) {
+    var formData = new FormData(form);
+    var arr=[];
+  
+    for (var pair of formData.entries()) {
+    if(pair[0]=="classes" && pair[1].includes(";")){
+        dict["classes"] = string2array(pair[1])
+    }
+    else if(pair[0]!="hub"){
+        dict[pair[0]] = pair[1];
+    }
+    else{
+        arr.push(pair[1]);
+    }
+      
+    }
+    dict["hub"] = arr;
+    console.log(dict);
+  }
+  
+  document.getElementById("myForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    getData(e.target);
+  });
+  
+const myJSON = JSON.stringify(dict);
