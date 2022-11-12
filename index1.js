e536929022814c12c8d123aa351cba988b88cccf
Tommy Ho <tommyho973@gmail.com>
2022-11-12 17:52:28 -0500

    Made a POST Request in index1.jsfile

--- index1.js
+++ index1.js
@@ -36,3 +36,14 @@ function getData(form) {
   });
   
 const myJSON = JSON.stringify(dict);
+let xhr = new XMLHttpRequest();
+xhr.open("POST", "");
+xhr.setRequestHeader("Accept", "application/json");
+xhr.setRequestHeader("Content-Type", "application/json");
+
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+    console.log(xhr.status);
+    console.log(xhr.responseText);
+  }};
+  xhr.send(data);
